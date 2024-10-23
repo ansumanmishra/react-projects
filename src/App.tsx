838395282d861eee25875a71b871a1bcb58a770a
AXM10014 <Ansuman.Mishra@netmodule.com>
2024-10-23 15:33:36 +0200

    Default react setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import Posts from './pages/Posts.tsx';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import PostsCached from './pages/PostsCached.tsx';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: true,
-      staleTime: 5 * 60 * 1000,
-      cacheTime: 10 * 60 * 1000,
-      retry: 3,
-    },
-    mutations: {
-      retry: 3,
-    }
-  },
-});
-
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>React query</h1>
-        <h3>Posts</h3>
-        <Posts/>
-
-        <h3>Posts cached</h3>
-        <PostsCached />
-      </div>
-    </QueryClientProvider>
+    <div>
+      <h1>App</h1>
+    </div>
   )
 }
 
